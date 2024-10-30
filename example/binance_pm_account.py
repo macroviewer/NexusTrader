94e01c78907f7796488c33e720f7ee74c15eb36f
River-Shi <nachuan.shi.quant@gmail.com>
2024-10-30 05:03:39 +0000

    Refactor Binance account management: update balance retrieval and enhance Asset structure

--- example/binance_pm_account.py
+++ example/binance_pm_account.py
@@ -1,18 +1,14 @@
 import asyncio
 import ccxt.pro as ccxt
-import msgspec
+
 from decimal import Decimal
-from pprint import pprint
+from tradebot.types import Asset
 from tradebot.constants import CONFIG
 from tradebot.exchange.binance import BinanceExchangeManager
 
 BINANCE_PM_API = CONFIG["binance_uni"]["API_KEY"]
 BINANCE_PM_SECRET = CONFIG["binance_uni"]["SECRET"]
 
-class Asset(msgspec.Struct):
-    asset: str
-    free: Decimal
-    borrowed: Decimal
 
 async def main():
     try:
@@ -27,41 +23,41 @@ async def main():
         }
         exchange = BinanceExchangeManager(config)
         api: ccxt.binance = exchange.api
-        res = await api.papi_get_account()
-        pprint(res)
-        # for asset in res:
-        #     if Decimal(asset['totalWalletBalance']) > 0:
-        #         print(asset)
-                # a = Asset(
-                #     asset=asset['asset'],
-                #     free=Decimal(asset['crossMarginAsset']),
-                #     borrowed=Decimal(asset['crossMarginBorrowed']),
-                # )
-                # print("Cross Margin Asset")
-                # print(a)
-                
-                # print("\nUM Asset")
-                # a = Asset(
-                #     asset=asset['asset'],
-                #     free=Decimal(asset['umWalletBalance']),
-                #     borrowed=Decimal(0),
-                # )
-                # print(a)
-                
-                # print("\nCM Asset")
-                # a = Asset(
-                #     asset=asset['asset'],
-                #     free=Decimal(asset['cmWalletBalance']),
-                #     borrowed=Decimal(0),
-                # )
-                # print(a)
-                # print("=====================================")
-       
-            
-        
-        
+
+        res = await api.papi_get_balance()
+        for asset in res:
+            if Decimal(asset["totalWalletBalance"]) > 0:
+                print(asset)
+                a = Asset(
+                    asset=asset["asset"],
+                    free=Decimal(asset["crossMarginFree"]),
+                    borrowed=Decimal(asset["crossMarginBorrowed"]),
+                    locked=Decimal(asset["crossMarginLocked"]),
+                )
+                print("Cross Margin Asset")
+                print(a)
+                print("Total: ", a.total)
+
+                print("\nUM Asset")
+                a = Asset(
+                    asset=asset["asset"],
+                    free=Decimal(asset["umWalletBalance"]),
+                    borrowed=Decimal(0),
+                )
+                print(a)
+
+                print("\nCM Asset")
+                a = Asset(
+                    asset=asset["asset"],
+                    free=Decimal(asset["cmWalletBalance"]),
+                    borrowed=Decimal(0),
+                )
+                print(a)
+                print("=====================================")
+
     finally:
         await exchange.close()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- tradebot/types.py
+++ tradebot/types.py
@@ -1,19 +1,7 @@
 from decimal import Decimal
-from dataclasses import dataclass, field
 from typing import Any, Dict
 from typing import Literal, Optional
-from msgspec import Struct
-
-
-# @dataclass(slots=True)
-# class BookL1:
-#     exchange: str
-#     symbol: str
-#     bid: float
-#     ask: float
-#     bid_size: float
-#     ask_size: float
-#     timestamp: int
+from msgspec import Struct, field
 
 
 class BookL1(Struct, gc=False):
@@ -26,34 +14,12 @@ class BookL1(Struct, gc=False):
     timestamp: int
 
 
-# @dataclass(slots=True)
-# class Trade:
-#     exchange: str
-#     symbol: str
-#     price: float
-#     size: float
-#     timestamp: int
-
-
 class Trade(Struct, gc=False):
     exchange: str
     symbol: str
     price: float
     size: float
     timestamp: int
-
-
-# @dataclass(slots=True)
-# class Kline:
-#     exchange: str
-#     symbol: str
-#     interval: str
-#     open: float
-#     high: float
-#     low: float
-#     close: float
-#     volume: float
-#     timestamp: int
 
 
 class Kline(Struct, gc=False):
@@ -68,28 +34,11 @@ class Kline(Struct, gc=False):
     timestamp: int
 
 
-# @dataclass(slots=True)
-# class MarkPrice:
-#     exchange: str
-#     symbol: str
-#     price: float
-#     timestamp: int
-
-
 class MarkPrice(Struct, gc=False):
     exchange: str
     symbol: str
     price: float
     timestamp: int
-
-
-# @dataclass(slots=True)
-# class FundingRate:
-#     exchange: str
-#     symbol: str
-#     rate: float
-#     timestamp: int
-#     next_funding_time: int
 
 
 class FundingRate(Struct, gc=False):
@@ -98,14 +47,6 @@ class FundingRate(Struct, gc=False):
     rate: float
     timestamp: int
     next_funding_time: int
-
-
-# @dataclass(slots=True)
-# class IndexPrice:
-#     exchange: str
-#     symbol: str
-#     price: float
-#     timestamp: int
 
 
 class IndexPrice(Struct, gc=False):
@@ -145,48 +86,35 @@ class Order(Struct):
     leverage: Optional[int] = None
 
 
-# @dataclass
-# class Order:
-#     raw: Dict[str, Any]
-#     success: bool
-#     exchange: str
-#     id: str
-#     client_order_id: str
-#     timestamp: int
-#     symbol: str
-#     type: Literal["limit", "market"]
-#     side: Literal["buy", "sell"]
-#     status: Literal[
-#         "new", "partially_filled", "filled", "canceled", "expired", "failed"
-#     ]
-#     price: Optional[float] = None
-#     average: Optional[float] = None
-#     last_filled_price: Optional[float] = None
-#     amount: Optional[Decimal] = None
-#     filled: Optional[Decimal] = None
-#     last_filled: Optional[Decimal] = None
-#     remaining: Optional[Decimal] = None
-#     fee: Optional[float] = None
-#     fee_currency: Optional[str] = None
-#     cost: Optional[float] = None
-#     last_trade_timestamp: Optional[int] = None
-#     reduce_only: Optional[bool] = None
-#     position_side: Optional[str] = None
-#     time_in_force: Optional[str] = None
-#     leverage: Optional[int] = None
+class Asset(Struct):
+    asset: str
+    free: Decimal
+    borrowed: Decimal
+    locked: Decimal = field(default=Decimal("0.0"))
 
-#     def __post_init__(self):
-#         decimal_fields = ["amount", "filled", "last_filled", "remaining"]
+    @property
+    def total(self) -> Decimal:
+        return self.free + self.locked
 
-#         for decimal_field in decimal_fields:
-#             if getattr(self, decimal_field) is not None and not isinstance(
-#                 getattr(self, decimal_field), Decimal
-#             ):
-#                 setattr(self, decimal_field, Decimal(str(getattr(self, decimal_field))))
+    def update_free(self, amount: Decimal):
+        """
+        if amount > 0, then it is a buying action
+        if amount < 0, then it is a selling action
+        """
+        self.free += amount
 
-#         float_fields = ["price", "average", "last_filled_price", "fee", "cost"]
-#         for float_field in float_fields:
-#             if getattr(self, float_field) is not None and not isinstance(
-#                 getattr(self, float_field), float
-#             ):
-#                 setattr(self, float_field, float(getattr(self, float_field)))
+    def update_borrowed(self, amount: Decimal):
+        """
+        if amount > 0, then it is a borrowing action
+        if amount < 0, then it is a repayment action
+        """
+        self.borrowed += amount
+        self.free += amount
+
+    def update_locked(self, amount: Decimal):
+        """
+        if amount > 0, then it is a new order action
+        if amount < 0, then it is a cancellation/filled/partially filled action
+        """
+        self.locked += amount
+        self.free -= amount
