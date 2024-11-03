f60f0ff0b9f623bc6cf25a8c9a622b8849fcfa5f
River-Shi <nachuan.shi.quant@gmail.com>
2024-11-03 02:47:24 +0000

    Refactor websocket clients and add example scripts

---
+++ example/binance_websocket_client.py
@@ -0,0 +1,28 @@
+import orjson
+import asyncio
+from tradebot.exchange.binance import BinanceAccountType
+from tradebot.exchange.binance.websockets import BinanceWebSocketClient
+
+
+def msg_handler(raw):
+    msg = orjson.loads(raw)
+    print(msg)
+
+async def main():
+    loop = asyncio.get_event_loop()
+    client = BinanceWebSocketClient(
+        account_type=BinanceAccountType.SPOT,
+        handler=msg_handler,
+        loop=loop,
+    )
+    
+    await client.connect()
+    
+    await client.subscribe_book_ticker("BTCUSDT")
+    
+    await asyncio.sleep(30)
+    
+    await client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
