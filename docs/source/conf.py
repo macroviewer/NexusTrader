b3ea38136eb52242cf0d66d48a571450b630b27e
River-Shi <nachuan.shi.quant@gmail.com>
2024-12-09 02:49:54 +0000

    Update Sphinx documentation configuration and add new logo files
    - Removed the SVG file exclusion from .gitignore to allow tracking of logo files.
    - Updated conf.py to specify light and dark logos for improved branding in the documentation.
    - Added new SVG logo files for both light and dark themes, enhancing visual consistency across different themes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,4 +20,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'furo'
 html_static_path = ['_static'] 
-html_logo = '_static/logo.png'
+html_theme_options = {
+    'light_logo': '_static/msgspec-logo-light.png',
+    'dark_logo': '_static/msgspec-logo-dark.png',
+}
