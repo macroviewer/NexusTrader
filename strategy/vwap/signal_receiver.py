14ebc0c3a5691e5878e0f8fb42e2aac53756aef9
River-Shi <nachuan.shi.quant@gmail.com>
2024-12-09 15:30:25 +0000

    Implement Bybit signal receiver and TWAP strategy with enhanced position management
    - Introduced new signal receiver in `signal_receiver.py` to handle asynchronous data from Bybit using ZeroMQ.
    - Added `strategy_bybit_twap_testnet.py` and `strategy_bybit_vwap.py` for implementing Time-Weighted Average Price (TWAP) strategies.
    - Enhanced position management logic to improve order handling and accuracy in `VwapStrategy`.
    - Updated `.gitignore` to exclude `.pkl` files and other unnecessary files.
    - Added test scripts for signal reception and server simulation to facilitate development and testing.

---
+++ strategy/vwap/signal_receiver.py
@@ -0,0 +1,44 @@
+import zmq.asyncio
+import orjson
+import asyncio
+from typing import Dict
+from tradebot.exchange.bybit.types import BybitMarket
+from tradebot.exchange.bybit import BybitExchangeManager
+from decimal import Decimal
+
+
+class BybitSignal:
+    def __init__(self, market: Dict[str, BybitMarket] = None):
+        context = zmq.asyncio.Context()
+        self.socket = context.socket(zmq.SUB)
+        self.socket.connect("ipc:///tmp/zmq_data")
+        self.socket.setsockopt(zmq.SUBSCRIBE, b"")
+        self.market = market
+
+    async def receive(self):
+        while True:
+            pos = {}
+            response = await self.socket.recv()
+            data = orjson.loads(response)
+            for d in data:
+                try:
+                    symbol: str = d["instrumentID"]
+                    symbol = symbol.replace("USDT.BBP", "/USDT:USDT")
+                    pos[symbol] = Decimal(str(d["position"])) * Decimal(str(self.market[symbol].precision.amount))
+                except Exception as e:
+                    print(e, d)
+            print(pos)
+            # EventSystem.emit("signal", pos)
+
+
+async def main():
+    try:
+        exchange = BybitExchangeManager()
+        signal = BybitSignal(exchange.market)
+        await signal.receive()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
