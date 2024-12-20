f3647fc52f6bb5ca4edfd40ad6022e9c21699671
chase <cjx463045792@gmail.com>
2024-12-20 21:32:43 +0800

    Remove obsolete test files and redundant market data loading logic
    - Deleted test files for AsyncHttpRequests, BinanceOrderManager, and entity tests to streamline the codebase and improve maintainability.
    - Removed the market data loading logic for Binance and OKX from the test initialization, simplifying the setup process.
    - This cleanup enhances the overall organization of the test suite and reduces unnecessary complexity in the codebase.

--- test/__init__.py
+++ test/__init__.py
@@ -1,25 +1,3 @@
-import ccxt
-import json
-import os
-
-file_path = os.path.join("test", "test_data", "binance_markets.json")
-
-binance = ccxt.binance()
-markets = binance.load_markets()
-
-if not os.path.exists(file_path):
-    with open(file_path, "w") as f:
-        json.dump(markets, f, indent=2)
-
-file_path = os.path.join("test", "test_data", "okx_markets.json")
-
-okx = ccxt.okx()
-markets = okx.load_markets()
-
-if not os.path.exists(file_path):
-    with open(file_path, "w") as f:
-        json.dump(markets, f, indent=2)
-
 
 
 
