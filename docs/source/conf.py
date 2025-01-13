acc2ca04f289412836c07d3793907ed603a804d9
River-Shi <nachuan.shi.quant@gmail.com>
2025-01-13 15:35:21 +0000

    Update Sphinx configuration to include 'msgspec' and 'nautilus_trader' in autodoc_mock_imports
    - Added 'msgspec' and 'nautilus_trader' to the autodoc_mock_imports list in the Sphinx configuration file.
    - This change aims to prevent import issues during documentation generation and enhance the reliability of the documentation process.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,4 +44,4 @@ autodoc_default_options = {
 todo_include_todos = True
 
 # Mock modules that might cause import issues
-autodoc_mock_imports = ['redis', 'aioredis', 'ccxt', 'ccxt.pro', 'msgepec', 'dynaconf', 'spdlog']
+autodoc_mock_imports = ['redis', 'aioredis', 'ccxt', 'ccxt.pro', 'msgspec', 'dynaconf', 'spdlog', 'nautilus_trader']
